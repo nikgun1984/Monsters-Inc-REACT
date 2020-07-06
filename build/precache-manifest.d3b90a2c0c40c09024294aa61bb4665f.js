bed13f503d3a70bd2f15cff89ad899316c30254f
Nick Gundobin <ngundobin@gmail.com>
2020-07-06 15:48:26 -0400

    Components Functions and Class
    Difference between function and class Component on the simple application

---
+++ build/precache-manifest.d3b90a2c0c40c09024294aa61bb4665f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d050a7c04c1dc4674b93d5f614133e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b20a9327a84bd98a1ac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3103ab1e3a9e55a70d11",
+    "url": "/static/js/2.6c832960.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c832960.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b20a9327a84bd98a1ac",
+    "url": "/static/js/main.8fd4da45.chunk.js"
+  },
+  {
+    "revision": "321be40e4d0b3008f64a",
+    "url": "/static/js/runtime-main.c34bb27e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
